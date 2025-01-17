abc982df70054fb455454058e5a76ee9acd5867e
hardtrip-jpg <user@hardtrip.ca>
2025-01-17 00:36:23 -0500

    task 7 complete

--- in-class/js-review/js/script.js
+++ in-class/js-review/js/script.js
@@ -4,27 +4,61 @@ let current_color = {
     r: 255,
     b: 255,
     g: 255,
-}
+};
 
-let current_state = "flip"
+const object_size = 50;
+
+let current_state = "circle";
+
+let row_count = 0
 
 function setup() {
     console.log("go");
-    createCanvas(640, 480);
-
+    createCanvas(500, 500);
+    row_count = width / object_size
+    
 }
 
 function draw() {
     background("#000000");
+    push();
+    fill(current_color.r,current_color.b,current_color.g);
 
+    for (let i = 0; i < object_size; i++){
+        switch(current_state){
+            case "circle":
+                drawCircleRow(object_size * i);
+                current_state = "square"
+                break;
+            case "square":
+                drawSquareRow(object_size * i);
+                current_state = "circle"
+                break;
+        }
+    }
+    pop();
+}
+
+function drawCircleRow(cur_y){
+    for(let i = 0; i < object_size; i++){
+        ellipse(object_size * i, cur_y, object_size);
+    }
+}
+
+function drawSquareRow(cur_y){
+    for(let i = 0; i < object_size; i++){
+        rect((object_size * i) - (object_size/2), cur_y - (object_size/2), object_size);
+    }
 }
 
 function mouseClicked() {
     switch (current_state) {
-        case "flip":
-            current_state = "flop";
-        case "flop":
-            current_state = "flip";
+        case "circle":
+            current_state = "square";
+            break;
+        case "square":
+            current_state = "circle";
+            break;
     }
 }
 
---
+++ in-class/js-review/js/task7.js
@@ -0,0 +1,71 @@
+"use strict";
+
+let current_color = {
+    r: 255,
+    b: 255,
+    g: 255,
+};
+
+const object_size = 50;
+
+let current_state = "circle";
+
+let row_count = 0
+
+function setup() {
+    console.log("go");
+    createCanvas(500, 500);
+    row_count = width / object_size
+    
+}
+
+function draw() {
+    background("#000000");
+    push();
+    fill(current_color.r,current_color.b,current_color.g);
+
+    for (let i = 0; i < object_size; i++){
+        switch(current_state){
+            case "circle":
+                drawCircleRow(object_size * i);
+                current_state = "square"
+                break;
+            case "square":
+                drawSquareRow(object_size * i);
+                current_state = "circle"
+                break;
+        }
+    }
+    pop();
+}
+
+function drawCircleRow(cur_y){
+    for(let i = 0; i < object_size; i++){
+        ellipse(object_size * i, cur_y, object_size);
+    }
+}
+
+function drawSquareRow(cur_y){
+    for(let i = 0; i < object_size; i++){
+        rect((object_size * i) - (object_size/2), cur_y - (object_size/2), object_size);
+    }
+}
+
+function mouseClicked() {
+    switch (current_state) {
+        case "circle":
+            current_state = "square";
+            break;
+        case "square":
+            current_state = "circle";
+            break;
+    }
+}
+
+function keyPressed() {
+    if (key === " ") {
+        current_color.r = random(0, 255);
+        current_color.b = random(0, 255);
+        current_color.g = random(0, 255);
+    }
+}
