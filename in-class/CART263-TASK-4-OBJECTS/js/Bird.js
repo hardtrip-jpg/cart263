f527792c78b13f19f643f29a31c617973b3e291f
hardtrip <user@hardtrip.ca>
2025-02-20 16:09:02 -0500

    birds

---
+++ in-class/CART263-TASK-4-OBJECTS/js/Bird.js
@@ -0,0 +1,23 @@
+class Bird {
+    constructor(x, y, width, height, color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = Math.floor(Math.random() * 16777215).toString(16);;
+        if (color) {
+            this.color = color;
+        }
+        this.birdDiv = document.createElement('div')
+
+    }
+
+    Bird() {
+
+    }
+
+    animateBird() {
+
+    }
+
+}
